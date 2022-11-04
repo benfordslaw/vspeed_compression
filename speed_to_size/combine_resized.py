e172d8062f92006a2a655d915194a8627466a665
Benford Krummenacher <74384808+benfordslaw@users.noreply.github.com>
2022-11-04 16:47:53 -0400

    initial code dump

---
+++ speed_to_size/combine_resized.py
@@ -0,0 +1,89 @@
+import sys
+sys.path.append('/Users/benkrummenacher/opt/anaconda3/envs/vpype_env/lib/python3.10/site-packages')
+
+import cv2 as cv
+from numpy.lib.function_base import average
+import random
+import numpy as np
+import glob
+from tqdm import tqdm
+import matplotlib.pyplot as plt
+import math
+
+filename = sys.argv[1]
+thres = int(sys.argv[2])
+frame_dist = int(sys.argv[3])
+ds_factor = float(sys.argv[4])
+min_mag_r = float(sys.argv[5])
+max_mag_r = float(sys.argv[6])
+pad = 25
+
+def takeSecond(elem):
+	return elem[1]
+
+def init_normalize(total_frames, cap, sample_size):
+	accum_mag = []
+
+	for _ in tqdm(range(sample_size)):
+
+		f_ct = random.randint(0, total_frames-frame_dist-1)
+
+		cap.set(1, f_ct)
+		_, f1 = cap.read()
+		cap.set(1, f_ct + frame_dist)
+		_, f2 = cap.read()
+
+		prvs = cv.cvtColor(f1, cv.COLOR_BGR2GRAY) 
+		next = cv.cvtColor(f2, cv.COLOR_BGR2GRAY) 
+
+		mag = get_mag(prvs, next)
+
+		accum_mag.extend(list(map(int, mag.flatten())))
+
+	return(np.max(accum_mag))
+
+
+def get_mag(prvs, next):
+	flow = cv.calcOpticalFlowFarneback(prvs, next, None, 0.5, 1, thres, 3, 5, 1.2, 0)
+	mag, _ = cv.cartToPolar(flow[..., 0], flow[..., 1])
+
+	return mag
+
+def main():
+	cap = cv.VideoCapture(cv.samples.findFile(filename))
+	total = int(cap.get(7))
+
+	_, fast_img = cap.read()
+	prvs = cv.cvtColor(fast_img, cv.COLOR_BGR2GRAY)
+
+	max_mag = init_normalize(total, cap, 100)
+	(txt_width, txt_height), baseline = cv.getTextSize(filename, cv.FONT_HERSHEY_PLAIN, 1, 1)
+
+	for frame_ct in tqdm(np.arange(0, total-1, frame_dist)):
+		accum_img = np.zeros_like(fast_img)
+
+		cap.set(1, frame_ct + frame_dist)
+		_, f2 = cap.read()
+
+		next = cv.cvtColor(f2, cv.COLOR_BGR2GRAY) 
+
+		mag = get_mag(prvs, next)
+		min_mag_allowed = max_mag * min_mag_r 
+		max_mag_allowed = max_mag * max_mag_r
+
+		for i in range(mag.shape[0]):
+			for j in range(mag.shape[1]):
+				if mag[i][j] > min_mag_allowed and mag[i][j] < max_mag_allowed:
+					h_ds = (ds_factor * mag.shape[0] * mag[i][j] / max_mag) + 1
+					v_ds = (ds_factor * mag.shape[1] * mag[i][j] / max_mag) + 1
+					h_idx = math.floor(i/h_ds + mag.shape[0] * (h_ds - 1) / (2 * h_ds))
+					v_idx = math.floor(j/v_ds + mag.shape[1] * (v_ds - 1) / (2 * v_ds))
+					accum_img[h_idx][v_idx] = f2[i][j]
+		pad_img = cv.copyMakeBorder(accum_img, pad, pad, pad, pad, cv.BORDER_CONSTANT)
+		txt_img = cv.putText(pad_img, filename, (pad, pad + mag.shape[0] + txt_height+baseline), cv.FONT_HERSHEY_PLAIN, 1, (0,255,255), 1, cv.LINE_4)
+		cv.imwrite("output/" + str(thres) + "_" + str('{:0>6}'.format(frame_ct)) + ".png", txt_img)
+
+		prvs = cv.cvtColor(f2, cv.COLOR_BGR2GRAY) 
+
+if __name__ == "__main__":
+	main()
---
+++ updating-only-fast-objects/combine_sep_speed.py
@@ -0,0 +1,99 @@
+import sys
+sys.path.append('/Users/benkrummenacher/opt/anaconda3/envs/vpype_env/lib/python3.10/site-packages')
+
+import cv2 as cv
+from numpy.lib.function_base import average
+import random
+import numpy as np
+import glob
+from tqdm import tqdm
+import matplotlib.pyplot as plt
+
+thres = int(sys.argv[2])
+frame_dist = int(sys.argv[3])
+depth = int(sys.argv[4])
+
+def takeSecond(elem):
+    return elem[1]
+
+def get_mean_mags(accum_means, mag_in):
+    print(len(accum_means))
+    if len(accum_means) < 2^depth + 1:
+        new_mag = []
+        for mag in mag_in:
+            mean = np.mean(mag)
+            accum_means.extend([mean])
+            mag_l = [m_el for m_el in mag if m_el < mean]
+            mag_h = [m_el for m_el in mag if m_el >= mean]
+            new_mag.extend([mag_l, mag_h])
+        return get_mean_mags(accum_means, new_mag) 
+    else:
+        accum_means.sort()
+        return accum_means
+
+def init_normalize(total_frames, cap, sample_size):
+    accum_mag = []
+
+    for _ in tqdm(range(sample_size)):
+
+        f_ct = random.randint(0, total_frames-frame_dist-1)
+
+        cap.set(1, f_ct)
+        _, f1 = cap.read()
+        cap.set(1, f_ct + frame_dist)
+        _, f2 = cap.read()
+
+        prvs = cv.cvtColor(f1, cv.COLOR_BGR2GRAY) 
+        next = cv.cvtColor(f2, cv.COLOR_BGR2GRAY) 
+
+        mag = get_mag(prvs, next)
+
+        accum_mag.extend(list(map(int, mag.flatten())))
+
+    accum_means = get_mean_mags([], [accum_mag])
+    accum_means.insert(0, 0)
+    accum_means.append(np.max(accum_mag))
+
+    return(accum_means)
+
+
+def get_mag(prvs, next):
+    flow = cv.calcOpticalFlowFarneback(prvs, next, None, 0.5, 1, thres, 3, 5, 1.2, 0)
+    mag, _ = cv.cartToPolar(flow[..., 0], flow[..., 1])
+
+    return mag
+
+def main():
+    cap = cv.VideoCapture(cv.samples.findFile(sys.argv[1]))
+    total = int(cap.get(7))
+
+    _, fast_img = cap.read()
+    prvs = cv.cvtColor(fast_img, cv.COLOR_BGR2GRAY)
+
+    accum_means = init_normalize(total, cap, 50)
+    accum_img = fast_img
+
+    num_seps = len(accum_means)-1
+
+    for frame_ct in tqdm(np.arange(0, total-1, frame_dist)):
+        for k in range(len(accum_means)-1):
+            cap.set(1, (frame_ct * (num_seps - k))%total)
+            _, f1 = cap.read()
+
+            cap.set(1, (frame_ct * (num_seps - k) + frame_dist)%total)
+            _, f2 = cap.read()
+
+            prvs = cv.cvtColor(f1, cv.COLOR_BGR2GRAY) 
+            next = cv.cvtColor(f2, cv.COLOR_BGR2GRAY) 
+
+            mag = get_mag(prvs, next)
+
+            for i in range(mag.shape[0]):
+                for j in range(mag.shape[1]):
+                    if mag[i][j] >= accum_means[k] and mag[i][j] < accum_means[k+1]:
+                        accum_img[i][j] = f2[i][j]
+    
+        cv.imwrite("output/" + str(thres) + "_" + str('{:0>6}'.format(frame_ct)) + ".png", accum_img)
+
+if __name__ == "__main__":
+    main()
